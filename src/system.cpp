37a04a81069dccb1a67720eb32bacf49d80d9a6f
Hassan <oh91@windowslive.com>
2023-04-18 17:39:23 +0200

    docs: remove todos

--- src/system.cpp
+++ src/system.cpp
@@ -16,23 +16,16 @@ using std::set;
 using std::size_t;
 using std::string;
 using std::vector;
-/*You need to complete the mentioned TODOs in order to satisfy the rubric
-criteria "The student will be able to extract and display basic data about the
-system."
 
-You need to properly format the uptime. Refer to the comments mentioned in
-format. cpp for formatting the uptime.*/
-
-// TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() {
   processes_ = {};
   for (auto pid : LinuxParser::Pids()) {
     processes_.push_back(Process(pid));
   }
-  std::sort(processes_.begin(), processes_.end());
+  std::partial_sort(processes_.begin(), processes_.begin() + 10,
+                    processes_.end());
   return processes_;
 }
 
